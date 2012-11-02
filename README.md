bc16f6629ef4ec874bd66b25eb5a5d8c58cf88ce
vazha-asatiani <vazha.asatiani@jumpstart.ge>
2012-11-02 13:52:07 +0400

    initial

---
+++ README.md
@@ -0,0 +1,88 @@
+# CDown v1.0
+
+# DESCRIPTION
+
+CDown is a lightweight countdown based on javascript & jQuery.
+
+
+# INSTALL
+
+Just download and include cdown.js file.
+
+
+# USAGE
+
+You just need to select an element with jQuery and then run cdown, e.g.
+```javascript
+$('#myid').cdown(options);
+```
+
+
+# OPTIONS
+
+options is an object with 2 necessary and 2 optional properties.
+
+## Necessary Properties
+
+* __start__: Start date. Javascript date object or time in seconds.  
+e.g.
+```javascript
+new Date()
+```
+or
+```javascript
+Math.round(new Date().getTime() / 1000)
+```
+(because new Date().getTime() returns time in milliseconds).
+
+* __end__: End date. Javascript date object or time in seconds.  
+For example, if you want the end date to be 13:00 February 3, 2013, you should create a date object according to this:
+```javascript
+new Date(year, month, day, hours, minutes, seconds)
+```
+and note that month indexes in javascript start with 0, so the object for 13:00 February 3, 2013 will be:
+```javascript
+var end = new Date(2013, 1, 3, 13, 0, 0);
+```
+and if you want to get seconds:
+```javascript
+end = Math.round(end.getTime() / 1000);
+```
+
+
+## Optional Properties
+
+* __use_plain_text__: boolean  
+If set to true, innerHTML of the countdown element will be only text, no html tags, e.g. '10:02:03:06'.  
+If set to false, countdown function will use a template with tags for custom styling:
+```html
+<div class="cdown_p">
+      <div class="cdown_number cdown_days">00</div>
+      <div class="cdown_define"> labels.days </div>
+</div>
+<span class="cdown_colon">:</span>
+<div class="cdown_p">
+      <div class="cdown_number cdown_hours">00</div>
+      <div class="cdown_define"> labels.hours </div>
+</div>
+<span class="cdown_colon">:</span>
+<div class="cdown_p">
+      <div class="cdown_number cdown_minutes">00</div>
+      <div class="cdown_define"> labels.minutes </div>
+</div>
+<span class="cdown_colon">:</span>
+<div class="cdown_p">
+      <div class="cdown_number cdown_seconds">00</div>
+      <div class="cdown_define"> labels.seconds </div>
+</div>
+```
+
+* __labels__: an object with 4 properties: days, hours, minutes & seconds.  
+If you want to have labels under numbers, you can set the values for them with this option. But if __use_plain_text__ property is set to _true_, this won't work, of course.  
+Also, if you want to use the template but you don't want to have the labels, you can set the _display: none_ property for _.cdown_define_ class.
+```css
+.cdown_define {
+      display: none;
+}
+```
+
---
+++ cdown.js
@@ -0,0 +1,180 @@
+
+// start & end arguments must be in seconds
+
+$.prototype.cdown = function (options)
+{
+  var cdown =
+  {
+    math: Math,
+    options: options
+  };
+
+  cdown.options.use_plain_text = ('use_plain_text' in cdown.options) ? cdown.options.use_plain_text : true;
+  cdown.options.labels = ('labels' in cdown.options) ? cdown.options.labels :
+  {
+    days:    'Days',
+    hours:   'Hours',
+    minutes: 'Minutes',
+    seconds: 'Seconds'
+  };
+  
+  cdown.runtime = cdown.math.round(+ new Date() / 1000);
+
+  var d = {start: cdown.options.start, end: cdown.options.end};
+  for (i in d)
+  {
+    if (d[i] instanceof Date)
+    {
+      cdown[i] = cdown.math.round(+ d[i] / 1000);
+    }
+    else if (+ d[i] > 0)
+    {
+      cdown[i] = + d[i];
+    }
+    else
+    {
+      return;
+    }
+  }
+
+  cdown.element = this;
+
+  cdown.onehour = 3600;
+  cdown.oneday  = 24 * cdown.onehour;
+
+  cdown.date_intervals = function (n)
+  {
+    n = + n;
+    var val =
+    {
+      days    : '00',
+      hours   : '00',
+      minutes : '00',
+      seconds : '00'
+    };
+
+    if (n < 0)
+    {
+      return val;
+    }
+
+    val.days = cdown.math.floor(n / cdown.oneday);
+    n -= val.days * cdown.oneday;
+
+    val.hours = cdown.math.floor(n / cdown.onehour);
+    n -= val.hours * cdown.onehour;
+
+    val.minutes = cdown.math.floor(n / 60);
+    n -= 60 * val.minutes;
+
+    val.seconds = n;
+
+    for (i in val)
+    {
+      (val[i] >= 0 && val[i] < 10) && (val[i] = '0' + val[i]);
+    }
+
+    return val;
+  };
+
+  cdown.init_html = function ()
+  {
+    if (cdown.options.use_plain_text)
+    {
+      var html = '00:00:00:00';
+      cdown.element.html(html);
+    }
+    else
+    {
+      var html =
+          '<div class="cdown_p">' +
+          '  <div class="cdown_number cdown_days">00</div>' +
+          '  <div class="cdown_define">' + cdown.options.labels.days + '</div>' +
+          '</div>' +
+          '<span class="cdown_colon">:</span>' +
+          '<div class="cdown_p">' +
+          '  <div class="cdown_number cdown_hours">00</div>' +
+          '  <div class="cdown_define">' + cdown.options.labels.hours + '</div>' +
+          '</div>' +
+          '<span class="cdown_colon">:</span>' +
+          '<div class="cdown_p">' +
+          '  <div class="cdown_number cdown_minutes">00</div>' +
+          '  <div class="cdown_define">' + cdown.options.labels.minutes + '</div>' +
+          '</div>' +
+          '<span class="cdown_colon">:</span>' +
+          '<div class="cdown_p">' +
+          '  <div class="cdown_number cdown_seconds">00</div>' +
+          '  <div class="cdown_define">' + cdown.options.labels.seconds + '</div>' +
+          '</div>';
+
+      cdown.element.html(html);
+      cdown.children = cdown.element.find('.cdown_days, .cdown_hours, .cdown_minutes, .cdown_seconds');
+    }
+  }
+
+  cdown.update = function ()
+  {
+    var now = cdown.math.round(+ new Date() / 1000);
+    var diff = cdown.end - (cdown.start + (now - cdown.runtime));
+
+    var intervals = cdown.date_intervals(diff);
+
+    if (cdown.options.use_plain_text)
+    {
+      var html = intervals.days + ':' + intervals.hours + ':' + intervals.minutes + ':' + intervals.seconds; 
+      cdown.element.html(html);
+    }
+    else
+    {
+      cdown.children.each(function ()
+      {
+        var t = $(this);
+        var classes = t[0].className.split(' ');
+        for (i in classes)
+        {
+          if (classes[i] == 'cdown_number')
+          {
+            continue;
+          }
+          else
+          {
+            var index = classes[i].slice(6);
+          }
+        }
+        t.html(intervals[index]);
+      });
+    }
+
+    setTimeout(cdown.update, 999);
+  }
+
+  cdown.init_html();
+  cdown.update();
+
+  return cdown.element;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
